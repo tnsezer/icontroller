f250b7aa81841c56acceffdf5499269f77624127
Tan Sezer <tnsezer@gmail.com>
2020-02-04 01:47:44 +0100

    first commit

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
-#!/usr/bin/env php
 <?php
-use Symfony\Component\Console\Application;
-use App\Console\Command\PayrollCommand;
 
-$app = new Application();
-$app->add(new PayrollCommand());
-$app->run();
+require 'bootstrap.php';
